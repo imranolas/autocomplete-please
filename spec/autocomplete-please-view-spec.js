018b697f8edd043f20fda9e85831417ac82dd1a0
imranolas <imranolas@gmail.com>
2016-08-30 11:25:34 +0100

    Initial commit

---
+++ spec/autocomplete-please-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompletePleaseView from '../lib/autocomplete-please-view';
+
+describe('AutocompletePleaseView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
