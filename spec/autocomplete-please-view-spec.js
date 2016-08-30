95ec26111047ddaead5327d8cc56d11fea04de11
imranolas <imranolas@gmail.com>
2016-08-30 14:26:50 +0100

    First pass at working auto complete

--- spec/autocomplete-please-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AutocompletePleaseView from '../lib/autocomplete-please-view';
-
-describe('AutocompletePleaseView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
